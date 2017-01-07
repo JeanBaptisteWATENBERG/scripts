3edcc9af2d59e97e81e03849c6f93ffcf5a98ee9
Jean-Baptiste WATENBERG <jbwatenberg@juniorisep.com>
2017-01-07 17:07:08 +0100

    startup

---
+++ README.md
@@ -0,0 +1,3 @@
+#Scripts for running indigoapp faster on dply
+
+
---
+++ startup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git clone http://ec2-54-171-34-222.eu-west-1.compute.amazonaws.com/jbwatenberg/indigoapp.git
+cd indigoapp
+mvn clean install
+docker-compose up
